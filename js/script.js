b87931b535f9ee2c1a9f76b594afb126a7f5197c
gabxs <gabriella.7086038@aluno.mg.gov.br>
2026-03-19 14:19:01 +0000

    uptade js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let likeCount = 0;
+let curtido - false; // flag booleana
 
 function curtir() {
-  likeCount++;
+if(curtir -- false){
+     likeCount++;
+     curtido = true;
+  document.getElementById("likeCount").innerText = likeCount;
+}else{
+   likeCount--;
+   curtido = false;
   document.getElementById("likeCount").innerText = likeCount;
 }
+
+}
+
+  
+ 
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
