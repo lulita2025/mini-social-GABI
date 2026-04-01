a25d57cf34096e4429fbed9819689499e0a4d8ca
gabxs <gabriella.7086038@aluno.mg.gov.br>
2026-04-01 13:53:17 +0000

    salvamento rapido

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
+<script>
 let likeCount = 0;
-let curtido - false; // flag booleana
+let curtido = false;
+let descurtido = false;
 
-function curtir() {
-if(curtir -- false){
-     likeCount++;
-     curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-}else{
-   likeCount--;
-   curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
+const likeBtn = document.querySelector(".likeBtn");
+const dislikeBtn = document.querySelector(".dislikeBtn");
+const countSpan = document.querySelector(".likeCount");
+
+function atualizar() {
+  countSpan.innerText = likeCount;
 }
 
-}
+likeBtn.addEventListener("click", () => {
+  if (!curtido) {
+    likeCount++;
+    curtido = true;
+    likeBtn.classList.add("liked");
 
-  
- 
+    // se estava descurtido, remove
+    if (descurtido) {
+      descurtido = false;
+      dislikeBtn.classList.remove("disliked");
+      likeCount++;
+    }
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
+  } else {
+    likeCount--;
+    curtido = false;
+    likeBtn.classList.remove("liked");
+  }
+
+  atualizar();
+});
+
+dislikeBtn.addEventListener("click", () => {
+  if (!descurtido) {
+    likeCount--;
+    descurtido = true;
+    dislikeBtn.classList.add("disliked");
+
+    // se estava curtido, remove
+    if (curtido) {
+      curtido = false;
+      likeBtn.classList.remove("liked");
+      likeCount--;
+    }
+
+  } else {
+    likeCount++;
+    descurtido = false;
+    dislikeBtn.classList.remove("disliked");
+  }
+
+  atualizar();
+});
+</script>
